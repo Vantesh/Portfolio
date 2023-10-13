cb34faefa43b23daf2d9fccfcba13022ded55b6c
Vantesh <121645908+Vantesh@users.noreply.github.com>
2023-10-13 04:16:33 +0300

    Update iconfiy icons

--- src/lib/content/skills.ts
+++ src/lib/content/skills.ts
@@ -1,60 +1,58 @@
-import { SkillsSectionType } from '@/lib/types/sections'
-import { getId } from '@/lib/utils/helper'
+import { SkillsSectionType } from '@/lib/types/sections';
+import { getId } from '@/lib/utils/helper';
 
 export const skillsSection: SkillsSectionType = {
-    title: 'what i do',
-    skills: [
+  title: 'what i do',
+  skills: [
     {
-        id: getId(),
-        title: 'full stack development',
-        // animation lottie file: https://lottiefiles.com/
-        lottie: {
+      id: getId(),
+      title: 'full stack development',
+      // animation lottie file: https://lottiefiles.com/
+      lottie: {
         light: '/lotties/frontend.json',
         dark: '/lotties/frontend-dark.json',
-        },
-        points: [
+      },
+      points: [
         'Building responsive static websites using Next.js',
         'Building responsive Single Page Apps in React.js',
         'Building RESTful APIs in Express',
-        ],
-        softwareSkills: [
-          // iconify icons: https://icon-sets.iconify.design/
-        { name: 'html-5', icon: 'vscode-icons:file-type-html' },
-        { name: 'CSS-3', icon: 'vscode-icons:file-type-css' },
-        { name: 'sass', icon: 'vscode-icons:file-type-sass' },
-        { name: 'javaScript', icon: 'vscode-icons:file-type-js-official' },
+      ],
+      softwareSkills: [
+        // iconify icons: https://icon-sets.iconify.design/
+        { name: 'Linux', icon: 'logos:linux-tux' },
+        { name: 'python', icon: 'logos:python' },
+        { name: 'Bash', icon: 'logos:bash-icon' },
+        { name: 'javaScript', icon: 'skill-icons:javascript' },
         {
-            name: 'typeScript',
-            icon: 'vscode-icons:file-type-typescript-official',
+          name: 'typeScript',
+          icon: 'logos:typescript-icon-round',
         },
-        { name: 'python', icon: 'vscode-icons:file-type-python' },
         { name: 'nodejs', icon: 'logos:nodejs-icon' },
-        { name: 'reactjs', icon: 'logos:react' },
+        { name: 'CSS', icon: 'vscode-icons:file-type-css' },
         { name: 'nextjs', icon: 'logos:nextjs-icon' },
-          // { name: 'angularjs', icon: 'logos:angular-icon' },
         { name: 'tailwindcss', icon: 'logos:tailwindcss-icon' },
-        { name: 'redux', icon: 'logos:redux' },
+        { name: 'React', icon: 'logos:react' },
         { name: 'database', icon: 'vscode-icons:file-type-sql' },
-        { name: 'jest', icon: 'vscode-icons:file-type-jest' },
-        ],
+        { name: 'CLI', icon: 'devicon:powershell' },
+      ],
     },
     {
-        id: getId(),
-        title: 'UI/UX designing',
-        lottie: {
+      id: getId(),
+      title: 'UI/UX designing',
+      lottie: {
         light: '/lotties/designing.json',
         dark: '/lotties/designing-dark.json',
-        },
-        points: [
+      },
+      points: [
         'Experience in designing user-friendly interfaces with figma',
         'Experience in developing design systems and style guides',
         'Providing user-friendly design solutions',
-        ],
-        softwareSkills: [
+      ],
+      softwareSkills: [
         { name: 'figma', icon: 'logos:figma' },
         { name: 'adobe illustrator', icon: 'logos:adobe-illustrator' },
         { name: 'adobe photoshop', icon: 'logos:adobe-photoshop' },
-        ],
+      ],
     },
-    ],
+  ],
 };
